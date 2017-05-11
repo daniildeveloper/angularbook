e3a0849cb24b51c426581d11e519cff15f02af0c
daniiltserin <daniilborowkow@yandex.ru>
2017-05-11 14:01:52 +0600

    first bussines logic with greeting

--- app/greeting/greeting.js
+++ app/greeting/greeting.js
@@ -1,12 +1,18 @@
 var greeting = angular.module("myApp.greeting", [])
-.controller("GreetingController", function($scope) {
-    $scope.now = new Date();
-    $scope.greeting = "Hello";//set model name on scope
-});
+    .controller("GreetingController", function ($scope) {
+        $scope.now = new Date();
+        $scope.greeting = "Hello";//set model name on scope
+
+        $scope.helloMessage = ["Hello", "Bonjur", "Hola", "Ciao", "Hallo"];
+
+        $scope.getRandomHelloMessage = function () {
+            $scope.greeting = $scope.helloMessage[parseInt((Math.random() * $scope.helloMessage.length))];
+        }
+    });
 
 greeting.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/greeting', {
-      templateUrl: 'greeting/greeting.html',
-      controller: 'GreetingController'
+        templateUrl: 'greeting/greeting.html',
+        controller: 'GreetingController'
     });
-  }])
+}])
